679221431a14e0d0817b47166cc3a8af798ae997
BRYANA <BRYANAKITCHEN@GMAIL.COM>
2020-12-07 20:21:41 -0500

    add City and Dog classes

--- sql/setup.sql
+++ sql/setup.sql
@@ -3,3 +3,16 @@ CREATE TABLE artists (
     artist TEXT NOT NULL,
     genre TEXT
 );
+
+CREATE TABLE dogs (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name TEXT NOT NULL,
+    age INT CHECK (age > 0),
+    owner_name TEXT
+)
+
+CREATE TABLE cities (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    city TEXT NOT NULL,
+    state TEXT NOT NULL,
+)
